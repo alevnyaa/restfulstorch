6bc44de4d16c6e37e4d2dbbd2b918c43079e9459
alevnyaa <alevnyaa@gmail.com>
2017-03-04 11:51:58 +0000

    Fixed spelling mistake AlloyAny => AllowAny

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AlloyAny',
+        'rest_framework.permissions.AllowAny',
     ],
     'PAGE_SIZE': 10
 }
