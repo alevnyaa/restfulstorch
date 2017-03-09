141690b209e2a1485a6b5b60b2c990e2d8ef8960
alevnyaa <alevnyaa@gmail.com>
2017-03-09 16:51:57 +0000

    Changed production to postgresql, with separate settings files

--- config/settings/local.py
+++ config/settings/local.py
@@ -5,3 +5,13 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3_mazoo6w00+u_!a8=a3s6+!yzt4e)7x+u0%znkk4o_@&53f%m'
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,8 +3,21 @@ from .secret import SECRET_KEY
 
 DEBUG = True
 
+SECRET_KEY = 'fpsuhf8ahfa398pfhawafp389fh3a8hfuofhp9v'
+
 ALLOWED_HOSTS = [
         '127.0.0.1',
         'localhost',
         '95.85.27.32',
         ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'restfulstorch',
+        'USER': 'restfulstorchuser',
+        'PASSWORD': 'a123b432',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
